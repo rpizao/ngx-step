331c4ef24d6049194445011e2dcde4392fc6e5ad
Rafael Felipe da Silva Pizão <rpizao@tecgraf.puc-rio.br>
2021-08-02 11:07:09 -0300

    Removendo classe de serviço da lib (não foi necessária) e corrigindo detalhes informações do package.json

--- projects/ngx-step/src/public-api.ts
+++ projects/ngx-step/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-step
  */
 
-export * from './lib/services/ngx-step.service';
 export * from './lib/components/ngx-step.component';
 export * from './lib/ngx-step.module';
